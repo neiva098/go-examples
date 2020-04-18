488e30b2e397e71d23a9ce6e97edd3a28275fba5
Cristiano <neivacristiano@yahoo.com.br>
2020-04-18 18:35:41 -0300

    functions - multiple return

--- functions/main.go
+++ functions/main.go
@@ -11,6 +11,10 @@ func soma3(a, b, c int) int {
 	return a + b + c
 }
 
+func vals() (int, int) {
+    return 3, 7
+}
+
 func main() {
 
 	res := soma(1, 2)
@@ -18,4 +22,11 @@ func main() {
 
 	res = soma3(1, 2, 3)
 	fmt.Println("1+2+3 =", res)
+
+	a, b := vals()
+    fmt.Println(a)
+    fmt.Println(b)
+
+    _, c := vals()
+    fmt.Println(c)
 }
